06a201be1a94d451e3441fa3c9e795e1e05027b7
RobotCherries <zdip14@gmail.com>
2020-12-13 23:44:23 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -13,195 +13,73 @@ export default class App extends React.Component {
         <p>Start editing to see some magic happen :)</p>
 
         <br />
-        <hr />
         <br />
-
-        <NameForm />
-
-        <br />
-        <hr />
-        <br />
-
-        <EssayForm />
-
-        <br />
-        <hr />
-        <br />
-
-        <FlavorForm />
-
-        <br />
-        <hr />
-        <br />
-
-        <ReservationForm />
+        <Clock />
       </div>
     );
   }
 }
 
-export class NameForm extends React.Component {
+export class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { date: new Date() };
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
 
-  handleSubmit(event) {
-    alert("A name was submitted: " + this.state.value);
-    event.preventDefault();
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <h2>Hello, world!</h2>
+        <FormattedDate date={this.state.date} />
+      </div>
     );
   }
 }
 
-export class EssayForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "Please write an essay about your favorite DOM element."
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert("An essay was submitted: " + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Essay:
-          <textarea value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+// Simple function version
+export function FormattedDate(props) {
+  return <p>It is {props.date.toLocaleTimeString()}.</p>;
 }
 
-export class FlavorForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "coconut" };
+// Complex class version
+// export class FormattedDate extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+//     this.state = {
+//       formattedDate: this.props.date.toLocaleTimeString()
+//     };
+//   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+//   componentDidMount() {
+//     this.timerID = setInterval(() => this.update(), 1000);
+//   }
 
-  handleSubmit(event) {
-    alert("Your favorite flavor is: " + this.state.value);
-    event.preventDefault();
-  }
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+//   update() {
+//     this.setState({
+//       formattedDate: this.props.date.toLocaleTimeString()
+//     });
+//   }
 
-export class ReservationForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isGoing: true,
-      numberOfGuests: 2
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const name = target.name;
-
-    const value = target.type === "checkbox" ? target.checked : target.value;
-
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit(event) {
-    alert(
-      "You have succefully reserved a table for " +
-        this.state.numberOfGuests +
-        " people."
-    );
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Is going:
-          <input
-            name="isGoing"
-            type="checkbox"
-            checked={this.state.isGoing}
-            onChange={this.handleInputChange}
-          />
-        </label>
-
-        <br />
-
-        <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange}
-          />
-        </label>
-
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+//   render() {
+//     return <p>It is {this.state.formattedDate}.</p>;
+//   }
+// }
